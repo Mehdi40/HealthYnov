d18014e608b22c4d6e717351d06e7b39dcecd4e4
Mehdi Lafenetre <mehdi.lafenetre@signactif.com>
2018-01-08 15:18:22 +0100

    First commit

---
+++ HealthYnov/HealthYnov/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthYnov
+//
+//  Created by Mehdi LAFENETRE on 08/01/2018.
+//  Copyright © 2018 Ynov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
