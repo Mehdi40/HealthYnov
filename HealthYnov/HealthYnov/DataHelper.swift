8f1e94d4ed426dec8aeb469ff13ef8f3c918f416
Developer <developer@Developers-Mac.local>
2018-01-15 23:00:20 +0100

    Improve DataHelper

--- HealthYnov/HealthYnov/DataHelper.swift
+++ HealthYnov/HealthYnov/DataHelper.swift
@@ -33,31 +33,16 @@ public class DataHelper{
     
     public func fillAllTables(){
         emptySuccess()
+        emptyActivity()
+        emptyHealthMessages()
         fillActivity()
         fillHealthMessages()
         fillSuccess()
-        printActivity()
     }
     
     // Functions for each table
     
-    // Empty tables
-    
-    public func emptySuccess(){
-        
-        let successFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Success")
-        let primarySortDescriptor = NSSortDescriptor(key: "name", ascending: true)
-        
-        successFetchRequest.sortDescriptors = [primarySortDescriptor]
-        
-        let allSuccess = (try! context.fetch(successFetchRequest)) as! [Success]
-        
-        for success in allSuccess {
-                context.delete(success)
-        }
-        
-    }
-    
+
     // Fill the Activity table
     
     public func fillActivity(){
@@ -142,39 +127,39 @@ public class DataHelper{
             
             // Success Type : Walking distance
             
-            (name: "Distance - Level 1", desc: "Walk 1 km", icon: "steps-bronze", requirements: 1),
-            (name: "Distance - Level 2", desc: "Walk 5 km", icon: "steps-bronze", requirements: 5),
-            (name: "Distance - Level 3", desc: "Walk 10 km", icon: "steps-bronze", requirements: 10),
+            (name: "Distance - Level 1", desc: "Walk 1 km", icon: "distance-bronze", requirements: 1),
+            (name: "Distance - Level 2", desc: "Walk 5 km", icon: "distance-silver", requirements: 5),
+            (name: "Distance - Level 3", desc: "Walk 10 km", icon: "distance-gold", requirements: 10),
             
             // Sucess Type : Climb floors
             
-            (name: "Stairs - Level 1", desc: "Climb 2 floors", icon: "steps-bronze", requirements: 2),
-            (name: "Stairs - Level 2", desc: "Climb 5 floors", icon: "steps-bronze", requirements: 5),
-            (name: "Stairs - Level 3", desc: "Climb 10 floors", icon: "steps-bronze", requirements: 10),
+            (name: "Stairs - Level 1", desc: "Climb 2 floors", icon: "stairs-bronze", requirements: 2),
+            (name: "Stairs - Level 2", desc: "Climb 5 floors", icon: "stairs-silver", requirements: 5),
+            (name: "Stairs - Level 3", desc: "Climb 10 floors", icon: "stairs-gold", requirements: 10),
             
             // Success Type : Running
   
-            (name: "Running - Level 1", desc: "Run for a whole km...", icon: "steps-bronze", requirements: 1),
-            (name: "Running - Level 2", desc: "Run for 5 km", icon: "steps-bronze", requirements: 5),
-            (name: "Running - Level 3", desc: "Run for 10 km", icon: "steps-bronze", requirements: 10),
+            (name: "Running - Level 1", desc: "Run for a whole km...", icon: "running-bronze", requirements: 1),
+            (name: "Running - Level 2", desc: "Run for 5 km", icon: "running-silver", requirements: 5),
+            (name: "Running - Level 3", desc: "Run for 10 km", icon: "running-gold", requirements: 10),
             
             // Success Type : Swimming
             
-            (name: "Swimming - Level 1", desc: "Swim a lap", icon: "steps-bronze", requirements: 1),
-            (name: "Swimming - Level 2", desc: "Swim 5 laps", icon: "steps-bronze", requirements: 5),
-            (name: "Swimming - Level 3", desc: "Swim 10 laps", icon: "steps-bronze", requirements: 10),
+            (name: "Swimming - Level 1", desc: "Swim a lap", icon: "swimming-bronze", requirements: 1),
+            (name: "Swimming - Level 2", desc: "Swim 5 laps", icon: "swimming-silver", requirements: 5),
+            (name: "Swimming - Level 3", desc: "Swim 10 laps", icon: "swimming-gold", requirements: 10),
             
             // Success Type : Biking
             
-            (name: "Biking - Level 1", desc: "Ride a bike for a whole km...", icon: "steps-bronze", requirements: 1),
-            (name: "Biking - Level 2", desc: "Ride a bike for 5 km", icon: "steps-bronze", requirements: 5),
-            (name: "Biking - Level 3", desc: "Ride a bike for 10 km", icon: "steps-bronze", requirements: 10),
+            (name: "Biking - Level 1", desc: "Ride a bike for a whole km...", icon: "biking-bronze", requirements: 1),
+            (name: "Biking - Level 2", desc: "Ride a bike for 5 km", icon: "biking-silver", requirements: 5),
+            (name: "Biking - Level 3", desc: "Ride a bike for 10 km", icon: "biking-gold", requirements: 10),
             
             // Success Type : Workout
             
-            (name: "Crunch - Level 1", desc: "Do a total of 50 crunchs", icon: "steps-bronze", requirements: 50),
-            (name: "Crunch - Level 2", desc: "Do a total of 500 crunchs", icon: "steps-bronze", requirements: 500),
-            (name: "Crunch - Level 3", desc: "Do a total of 5000 crunchs", icon: "steps-bronze", requirements: 5000)
+            (name: "Crunch - Level 1", desc: "Do a total of 50 crunchs", icon: "workout-bronze", requirements: 50),
+            (name: "Crunch - Level 2", desc: "Do a total of 500 crunchs", icon: "workout-silver", requirements: 500),
+            (name: "Crunch - Level 3", desc: "Do a total of 5000 crunchs", icon: "workout-gold", requirements: 5000)
             
         ]
         
@@ -193,4 +178,59 @@ public class DataHelper{
     }
     
 }
+    
+    // Empty tables
+    
+    // Empty Success
+    
+    public func emptySuccess(){
+        
+        let successFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Success")
+        let primarySortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        
+        successFetchRequest.sortDescriptors = [primarySortDescriptor]
+        
+        let allSuccess = (try! context.fetch(successFetchRequest)) as! [Success]
+        
+        for success in allSuccess {
+            context.delete(success)
+        }
+        
+    }
+    
+    // Empty Activity
+    
+    public func emptyActivity(){
+        
+        let activityFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Activity")
+        let primarySortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        
+        activityFetchRequest.sortDescriptors = [primarySortDescriptor]
+        
+        let allActivity = (try! context.fetch(activityFetchRequest)) as! [Activity]
+        
+        for activity in allActivity {
+            context.delete(activity)
+        }
+        
+    }
+    
+    // Empty Health Messages
+    
+    public func emptyHealthMessages(){
+        
+        let healthmessageFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "HealthMessage")
+        let primarySortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        
+        healthmessageFetchRequest.sortDescriptors = [primarySortDescriptor]
+        
+        let allHealthmessages = (try! context.fetch(healthmessageFetchRequest)) as! [HealthMessage]
+        
+        for healthmessage in allHealthmessages {
+            context.delete(healthmessage)
+        }
+        
+    }
+    
+    
 }
