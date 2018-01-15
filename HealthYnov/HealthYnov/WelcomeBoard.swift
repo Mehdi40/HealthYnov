14cd75d80769a6db03a19c90a5d59e55460fca36
Developer <developer@Developers-Mac.local>
2018-01-15 13:36:59 +0100

    Progress on writing content

--- HealthYnov/HealthYnov/WelcomeBoard.swift
+++ HealthYnov/HealthYnov/WelcomeBoard.swift
@@ -7,9 +7,40 @@
 //
 
 import UIKit
+import CoreData
 
-class WelcomeBoard: UIViewController {
+class WelcomeBoardController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return height[row]
+    }
+    
 
+    
+    @IBOutlet weak var NicknameTF: UITextField!
+    @IBOutlet weak var HeightPicker: UIPickerView!
+    @IBOutlet weak var WeightPicker: UIPickerView!
+    @IBOutlet weak var GenderSl: UISegmentedControl!
+    @IBAction func SubmitButton(_ sender: Any) {
+        
+        let nicknameUser =  NicknameTF.text
+        let height = HeightPicker
+        let weight = WeightPicker
+        let
+    }
+    
+    let Weight = ["1,00", "1,05", ]
+    
+    func SignIN(nickname:String, height:Float, weight:Float, gender:Bool) {
+        
+  
+        
+        
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
