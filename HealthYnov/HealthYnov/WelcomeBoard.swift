87753d08a4a9666d4277477555ba2612a6efb105
Developer <developer@Developers-Mac.local>
2018-01-15 15:25:22 +0100

    Updating PickerView base

--- HealthYnov/HealthYnov/WelcomeBoard.swift
+++ HealthYnov/HealthYnov/WelcomeBoard.swift
@@ -11,29 +11,65 @@ import CoreData
 
 class WelcomeBoardController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    @IBOutlet weak var NicknameTF: UITextField!
+    @IBOutlet weak var HeightPicker: UIPickerView!
+    @IBOutlet weak var GenderSl: UISegmentedControl!
+    
+    let heightTotal: Double = 2.40
+    let heightStep: Double = 0.05
+    var heightValues: [Double] = [1.0]
+    var weightValues: [Double] = [40]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        while heightValues.last! < heightTotal {
+            heightValues.append(heightValues.last! + heightStep)
+        }
+        
+        print(heightValues)
+    }
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return height[row]
+        
+        if component == 0 {
+            return heightValues.count
+        }
+        
+        if component == 1 {
+            return weightValues.count
+        }
+        
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if component == 0 {
+            return String(heightValues[row])
+        }
+        
+        if component == 1 {
+            return String(weightValues[row])
+        }
+        
+        return nil
     }
     
 
     
-    @IBOutlet weak var NicknameTF: UITextField!
-    @IBOutlet weak var HeightPicker: UIPickerView!
-    @IBOutlet weak var WeightPicker: UIPickerView!
-    @IBOutlet weak var GenderSl: UISegmentedControl!
     @IBAction func SubmitButton(_ sender: Any) {
         
         let nicknameUser =  NicknameTF.text
         let height = HeightPicker
         let weight = WeightPicker
-        let
+        let gender = GenderSl
     }
     
-    let Weight = ["1,00", "1,05", ]
     
     func SignIN(nickname:String, height:Float, weight:Float, gender:Bool) {
         
