ccd63d7454e5b59f9f607ee9012790b6e594d83b
Developer <developer@Developers-Mac.local>
2018-01-16 14:06:49 +0100

    Adding switch in Tableview

--- HealthYnov/HealthYnov/SuccessList.swift
+++ HealthYnov/HealthYnov/SuccessList.swift
@@ -25,6 +25,7 @@ class SuccessListTableViewController: UIViewController, UITableViewDataSource {
         let fetchRequest: NSFetchRequest<Success> = Success.fetchRequest()
         success = try? context.fetch(fetchRequest)
         
+
         let fetchRequestGoals: NSFetchRequest<Goal> = Goal.fetchRequest()
         goal = try? context.fetch(fetchRequestGoals)
         
@@ -33,62 +34,64 @@ class SuccessListTableViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-//        return success?.count ?? 0
-        var nbCell = 0
+
         
-        switch(successListSegment.selectedSegmentIndex)
+        
+        
+        let selectedIndex = self.successListSegment.selectedSegmentIndex
+        switch selectedIndex
         {
         case 0:
-//        tableView.reloadData()
-        nbCell = success?.count ?? 0
-            break
+            return success?.count ?? 0
         case 1:
-//        tableView.reloadData()
-        nbCell = goal?.count ?? 0
-            break
-            
+            return goal?.count ?? 0
+        //Add other cases here
         default:
-            break
-            
+            return 0
         }
         
-        return nbCell
+        
     }
     
     // Cell
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? SuccessTableViewCell else {
-            fatalError("Wrong cell type")
-        }
-        
-
-        let suc = success?[indexPath.row]
-//        let goa = goal?[indexPath.row]
-        let imagePath = suc?.icon
-
-        
-        switch(successListSegment.selectedSegmentIndex)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? SuccessTableViewCell else { fatalError("Wrong cell type")}
+        let selectedIndex = self.successListSegment.selectedSegmentIndex
+        switch selectedIndex
         {
         case 0:
+            let suc = success?[indexPath.row]
+            let imagePath = suc?.icon
             cell.Picture.image = UIImage(named: "\(imagePath ?? "nil")")
             cell.Title.text = suc?.name
             cell.Desc.text = suc?.desc
             break
         case 1:
+            let goa = goal?[indexPath.row]
             cell.Picture.image = UIImage(named: "cookie")
-            cell.Title.text = suc?.name
-            cell.Desc.text = suc?.desc
+            cell.Title.text = goa?.name
+            cell.Desc.text = goa?.desc
             break
             
+        //Add other cases here
         default:
             break
-            
         }
-        
-
         return cell
-
     }
+    
+    @IBOutlet private weak var tableView: UITableView!
+
+    
+    @IBAction func valueChanged(_ sender: Any) {
+//                self.tableView?.reloadData()
+        DispatchQueue.main.async{
+            self.tableView?.reloadData()
+        }
+ }
+    
+
 }
 
