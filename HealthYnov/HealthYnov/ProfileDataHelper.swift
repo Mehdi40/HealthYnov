a08c789b113be7804b804611c089c3b929fccc98
Mehdi <mehdi.lafenetre@gmail.com>
2018-01-16 11:45:50 +0100

    Profil (#2)
    * Profile Page - Firsts contraints + Get DataCore User informations
    
    * Adding main constraints to the Profile view
    
    * Constraints OK and Debug of Data imports

---
+++ HealthYnov/HealthYnov/ProfileDataHelper.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileDataHelper.swift
+//  HealthYnov
+//
+//  Created by Mehdi LAFENETRE on 16/01/2018.
+//  Copyright © 2018 Ynov. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class ProfileDataHelper {
+    let context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext){
+        self.context = context
+    }
+    
+    public func fillAllTables(){
+        emptyUser()
+        fillUser()
+    }
+    
+    public func fillUser(){
+        
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "User", into: context) as! User
+        newUser.username = "Mehdi"
+    
+    do{
+        try context.save()
+    } catch {
+        print(error)
+    }
+}
+    
+    public func emptyUser(){
+        let userFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        let primarySortDescriptor = NSSortDescriptor(key: "username", ascending: true)
+        
+        userFetchRequest.sortDescriptors = [primarySortDescriptor]
+        
+        let allUser = (try! context.fetch(userFetchRequest)) as! [User]
+        
+        for user in allUser {
+            context.delete(user)
+        }
+    }
+}
---
+++ HealthYnov/HealthYnov/ProfileViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ProfileViewController.swift
+//  HealthYnov
+//
+//  Created by Mehdi LAFENETRE on 09/01/2018.
+//  Copyright © 2018 Ynov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class ProfileViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? ProfileTableViewCell
+            else {
+                fatalError("Wrong cell type")
+            }
+        
+        cell.title.text = "Ta Grand Mère"
+        cell.desc.text = "C'est ta grand-mère"
+        return cell
+    }
+    
+    
+    //let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    //lazy var managedContext : NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+    
+    var context: NSManagedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var user: [User]!
+    
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var level: UILabel!
+    @IBOutlet weak var age: UILabel!
+    @IBOutlet weak var height: UILabel!
+    @IBOutlet weak var weight: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fetchRequest: NSFetchRequest<User> = User.fetchRequest()
+        user = try? context.fetch(fetchRequest)
+        
+        //let result = try? managedContext.fetch(fetch)
+        //user = result!.first!
+        
+        print(user)
+        }
+        
+//        username.text = String(user.username)
+        
+        
+        // Do any additional setup after loading the view.
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
