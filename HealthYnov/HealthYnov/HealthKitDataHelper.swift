5838032699e61ee259a5bd1ba027612dfa01f200
Developer <developer@Developers-Mac.local>
2018-01-22 15:13:22 +0100

    Tries about HealthKit

--- HealthYnov/HealthYnov/HealthKitDataHelper.swift
+++ HealthYnov/HealthYnov/HealthKitDataHelper.swift
@@ -15,8 +15,28 @@ import HealthKit
 
 public class HealthKitDataHelper{
   
+    func retrieveStepCount(completion: @escaping (_ stepRetrieved: Double) -> Void) {
+        
+        //   Define the Step Quantity Type
+        let stepsCount = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)
+        
+        //   Get the start of the day
+        let date = Date()
+        let cal = Calendar(identifier: Calendar.Identifier.gregorian)
+        let newDate = cal.startOfDay(for: date)
+        
+        //  Set the Predicates & Interval
+        let predicate = HKQuery.predicateForSamples(withStart: newDate, end: Date(), options: .strictStartDate)
+        var interval = DateComponents()
+        interval.day = 1
+        
+        //  Perform the Query
+        let query = HKStatisticsCollectionQuery(quantityType: stepsCount!, quantitySamplePredicate: predicate, options: [.cumulativeSum], anchorDate: newDate as Date, intervalComponents:interval)
+
+            
 
 
+        
 
 }
-
+}
